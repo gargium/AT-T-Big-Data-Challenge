3ab4288698b99f1bc9baaaed7745b636dba38ac3
Rakshit Garg <raks.garg@gmail.com>
2016-08-25 15:33:00 -0700

    ooga booga wooga

--- chart.py
+++ chart.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, Markup, json
 income = "B19013_001E"
 price = "B25077_001E"
 
-with open('Data/2010/sanFrancisco2010.geojson') as sf2010:
+with open('Data/sanFrancisco2010.geojson') as sf2010:
     sf2010Data = json.load(sf2010)
 
     print sf2010Data["features"]
