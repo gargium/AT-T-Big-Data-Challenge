d25c82d300752a7b29fc1aacae2eba36e8dbb38d
Jason Yang <jasonyang96@ucla.edu>
2016-08-25 21:12:49 -0700

    got charts to be more error friendly

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from algorithm import algorithm
 app = Flask(__name__)
 bayArea = bayArea()
 algorithm = algorithm()
+SFBSIs = algorithm.BSI("SF")
+SCBSIs = algorithm.BSI("SC")
+SRBSIs = algorithm.BSI("SR")
 
 
 @app.route("/")
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/chart")
 def chart():
@@ -18,24 +21,25 @@ def chart():
     countyToTractMap["SF"] = bayArea.get_tracts("SF")
     countyToTractMap["SC"] = bayArea.get_tracts("SC")
 
-    return render_template('chart.html', bayArea=json.dumps(bayArea.get_bay_area()), countyTractMap=json.dumps(countyToTractMap))
+    return render_template('chart.html', bayArea=json.dumps(bayArea.get_bay_area()), 
+        countyTractMap=json.dumps(countyToTractMap),
+        sfBSIs = json.dumps(SFBSIs),
+        scBSIs = json.dumps(SCBSIs),
+        srBSIs = json.dumps(SRBSIs))
 
 @app.route("/map")
 def map():
-    SFBSIs = algorithm.BSI("SF")
-    SCBSIs = algorithm.BSI("SC")
-    SRBSIs = algorithm.BSI("SR")
-
-    return render_template('map.html', urlSF='static/Data/sanFrancisco/sanFrancisco2010.geojson',
-                           urlSR='static/Data/sanRamon/sanRamon2010.geojson',
-                           urlSC='static/Data/santaClara/santaClara2010.geojson',
-                           sfBSIs = json.dumps(SFBSIs),
-                           scBSIs = json.dumps(SCBSIs),
-                           srBSIs = json.dumps(SRBSIs))
+    return render_template('map.html',
+        urlSF='static/Data/sanFrancisco/sanFrancisco2010.geojson',
+        urlSR='static/Data/sanRamon/sanRamon2010.geojson',
+        urlSC='static/Data/santaClara/santaClara2010.geojson',
+        sfBSIs = json.dumps(SFBSIs),
+        scBSIs = json.dumps(SCBSIs),
+        srBSIs = json.dumps(SRBSIs))
 
 @app.route("/about/")
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
